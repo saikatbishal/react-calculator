9c97f17a00c1d399c42e3c57df9be2e3021372b1
subratabishal <subratabishal10@gmail.com>
2021-10-30 09:43:34 +0530

    main-commit

---
+++ src/new.jsx
@@ -0,0 +1,10 @@
+import App from "./App";
+const Container = () => {
+    return (
+    <div>
+            {/* <h2>This is Hello world</h2> */}
+            <App></App>
+        
+    </div>
+)}
+export { Container };
